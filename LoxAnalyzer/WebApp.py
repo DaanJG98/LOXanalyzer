d5115da5678d7f71f7a203380e7185c43a82e761
DaanJG <dj.gilissen@student.han.nl>
2017-05-30 20:26:26 +0200

    query toegevoegd

--- LoxAnalyzer/WebApp.py
+++ LoxAnalyzer/WebApp.py
@@ -11,28 +11,26 @@ def index():
 
 @app.route('/dbsearch', methods=['POST', 'GET'])
 def search():
-    resultslist = list()
     text = request.form["searchword"]
 
     db = cx_Oracle.connect('owe7_pg2', 'blaat1234', 'cytosine.nl:1521/XE')
     cursor = db.cursor()
-    # cursor.execute('''SELECT SOORT_LOX.NAAM, APPLICATIE.NAAM, PUBLICATIE.PUBMEDID, PUBLICATIE.DATUM, AUTEUR.NAAM, KEYWORDS.WOORD, ORGANISME.NAAM, SEQUENTIE.VERSION_ID
-    #                FROM SOORT_LOX, APPLICATIE, PUBLICATIE, AUTEUR, KEYWORDS, ORGANISME, SEQUENTIE
-    #
-    #                ''')
+    cursor.execute('''
+                    SELECT SOORT_LOX.NAAM, APPLICATIE.NAAM, PUBLICATIE.PUBMEDID, PUBLICATIE.DATUM, AUTEUR.NAAM, KEYWORDS.WOORD, ORGANISME.NAAM, SEQUENTIE.VERSION_ID
+                    FROM SOORT_LOX, APPLICATIE, PUBLICATIE, AUTEUR, KEYWORDS, ORGANISME, SEQUENTIE, RELATION_2, RELATION_3, RELATION_6, RELATION_7
+                    WHERE APPLICATIE.APPLICATIE_ID = RELATION_6.APPLICATIE_APPLICATIE_ID AND SOORT_LOX.SOORT_LOX_ID = RELATION_6.SOORT_LOX_SOORT_LOX_ID
+                    AND PUBLICATIE.PUBLICATIE_ID = RELATION_7.PUBLICATIE_PUBLICATIE_ID AND APPLICATIE.APPLICATIE_ID = RELATION_7.APPLICATIE_APPLICATIE_ID
+                    AND PUBLICATIE.PUBLICATIE_ID = RELATION_2.PUBLICATIE_PUBLICATIE_ID AND AUTEUR.AUTEUR_ID = RELATION_2.AUTEUR_AUTEUR_ID
+                    AND KEYWORDS.KEYWORDS_ID = RELATION_3.KEYWORDS_KEYWORDS_ID AND PUBLICATIE.PUBLICATIE_ID = RELATION_3.PUBLICATIE_PUBLICATIE_ID
+                    AND ORGANISME.ORGANISME_ID = SEQUENTIE.ORGANISME_ORGANISME_ID
+                    AND SEQUENTIE.SOORT_LOX_SOORT_LOX_ID = SOORT_LOX.SOORT_LOX_ID
+                    ''')
 
-    # cursor.execute(''' SELECT AUTEUR.NAAM
-    #                    FROM RELATION_2, PUBLICATIE, AUTEUR
-    #                    WHERE PUBLICATIE.PUBLICATIE_ID = PUBLICATIE_PUBLICATIE_ID AND AUTEUR.AUTEUR_ID = AUTEUR_AUTEUR_ID
-    #                 ''')
-
-    cursor.execute(''' SELECT SOORT_LOX.NAAM, APPLICATIE.NAAM
-                       FROM RELATION_6, APPLICATIE, SOORT_LOX
-                       WHERE APPLICATIE.APPLICATIE_ID = APPLICATIE_APPLICATIE_ID AND SOORT_LOX.SOORT_LOX_ID = SOORT_LOX_SOORT_LOX_ID
-    ''')
     result = cursor.fetchall()
-    for regel in result:
-        print(regel)
+    result = [list(row) for row in result]
+    print(result)
+    results[0][0] += ', 15-LOX'
+    print(result)
 
     demolijst = [('13-LOX', 'Bleken', '27403427', '2017', 'Gilissen D.', 'defense, herbivore, oxylipin', 'Kutkikker', 'AOM81152.1'),
                  ('15-LOX', 'Bleken', '27403427',  '2015', 'Rademaker K.', 'defense, herbivore, oxylipin', 'Ander beest',
